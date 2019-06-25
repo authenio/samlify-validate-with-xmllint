ff7778eb063bd812dc2348176db4956563ebc63e
tngan <tonynwk919@gmail.com>
2019-06-26 03:33:07 +0900

    Export without default for commonjs

--- index.ts
+++ index.ts
@@ -3,19 +3,16 @@ import * as path from 'path';
 
 const xsd = 'saml-schema-protocol-2.0.xsd';
 
-export default {
-  validate: (xml: string) => {
-    return new Promise((resolve, reject) => {
+export const validate = (xml: string) => {
+  return new Promise((resolve, reject) => {
+    process.chdir(path.resolve(__dirname, './schemas'));
+    mod.validateXMLWithXSD(xml, xsd)
+      .then((result: any) => {
+        return resolve('SUCCESS_VALIDATE_XML');
+      }, (err: any) => {
+        console.error('[ERROR] validateXML', err);
+        return reject('ERR_INVALID_XML');
+      });
 
-      process.chdir(path.resolve(__dirname, './schemas'));
-      mod.validateXMLWithXSD(xml, xsd)
-        .then((result: any) => {
-          return resolve('SUCCESS_VALIDATE_XML');
-        }, (err: any) => {
-          console.error('[ERROR] validateXML', err);
-          return reject('ERR_INVALID_XML');
-        });
-
-    });
-  }
+  });
 };
